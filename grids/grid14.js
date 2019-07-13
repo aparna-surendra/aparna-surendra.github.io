5db359e74b30d641c2ef8be7cb26850a2a8170d9
Aparna Surendra <aparnas@alumni.stanford.edu>
2019-07-13 21:49:23 +0530

    new pages

--- grids/grid14.js
+++ grids/grid14.js
@@ -188,7 +188,7 @@ var column = row.selectAll(".square")
 	  	   grid.append("text")
 	  			.attr("class", "text_remove")
 		    	.attr("x", 215)
-		    	.attr("y", 248)
+		    	.attr("y", 137)
 		    	.text(function(d,i){return  "CUMULATIVE DISCOUNTED REWARD: " + cumulative_disc_reward;})
 		    	.style("font-weight", "bold")
 
